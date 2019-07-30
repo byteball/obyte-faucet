2b5dc7bfcbf70d3239678d572afa37e71dd50d3c
tony <tonyofbyteball@users.noreply.github.com>
2019-07-30 22:41:42 +0300

    higher amounts

--- conf.js
+++ conf.js
@@ -16,8 +16,8 @@ exports.permanent_pairing_secret = '0000';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
 exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 
-exports.MIN_AMOUNT_IN_KB = 50;
-exports.MAX_AMOUNT_IN_KB = 100;
+exports.MIN_AMOUNT_IN_KB = 50000;
+exports.MAX_AMOUNT_IN_KB = 100000;
 
 exports.KEYS_FILENAME = 'keys.json';
 
