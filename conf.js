77a87d5a8ef9bb6e5620fbf8526d94dd65ec9a69
tony <tonyofbyteball@users.noreply.github.com>
2020-08-09 01:24:38 +0300

    static change address and consolidation

--- conf.js
+++ conf.js
@@ -19,6 +19,10 @@ exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 exports.MIN_AMOUNT_IN_KB = 0.5e6;
 exports.MAX_AMOUNT_IN_KB = 1e6;
 
+exports.bStaticChangeAddress = true;
+exports.MAX_UNSPENT_OUTPUTS = 1;
+exports.CONSOLIDATION_INTERVAL = 3600*1000;
+
 exports.KEYS_FILENAME = 'keys.json';
 
 console.log('finished faucet conf');
